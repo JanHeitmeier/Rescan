ce359942371f1a5c9ccc27a338a9b70dcfdbf9b9
Jan Heitmeier <158503821+JanHeitmeier@users.noreply.github.com>
2024-12-01 13:59:56 +0100

    init

---
+++ app/src/main/java/de/rescan/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package de.rescan.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+
+val Beige = Color(0xFFFFFFCB)
+val GreenLight = Color(0xFFC7FFD2)
+val Turquoise = Color(0xFF72CEB7)
+val GreenDark = Color(0xFF2C858D)
+val BlueDark = Color(0xFF004157)
